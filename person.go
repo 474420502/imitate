cbfb3c605fd318fee64ada36e4c839f9a6bcf68e
huangsimin <huangsimin@youmi.net>
2018-11-05 18:36:15 +0800

    TODO: 用python的脚本 把所有模块的function添加到go的集合里面

--- person.go
+++ person.go
@@ -1,6 +1,11 @@
 package main
 
-import "path/filepath"
+import (
+	"path/filepath"
+
+	"github.com/474420502/requests"
+	py "github.com/sbinet/go-python"
+)
 
 // TypeMode 多种模式
 // 1. 以Proxy为个例
@@ -18,41 +23,52 @@ const (
 
 // Person 拥有两种类型并发任务状态, 一种是以cookie为个例控制, 另一种是以 代理为个例子.
 type Person struct {
-	mode  TypeMode
 	Tasks []Task
 }
 
 // NewPerson 创建一个新Person
-func NewPerson() *Person {
+func NewPerson(tpath string) *Person {
 	p := Person{}
 
-	matches, err := filepath.Glob("task/*_config.py")
+	matches, err := filepath.Glob(tpath)
 	if err != nil {
 		panic(err)
 	}
 
-	var tasks []*Task
+	var tasks []Task
 	for _, match := range matches {
 		task := NewTask(match)
-		tasks = append(tasks, task)
+
+		switch task.Config.Setting.Mode {
+		case ModeCookie:
+			tasks = append(tasks, *task)
+		case ModeProxy:
+			for _, t := range task.SplitFromProxies() {
+				tasks = append(tasks, t)
+			}
+		}
 	}
-
-	switch m {
-	case ModeCookie:
-
-	case ModeProxy:
-
-	}
+	p.Tasks = tasks
 
 	return &p
 }
 
-// SetMode 设置任务的模式
-// func (p *Person) SetMode(m TypeMode) {
-// 	p.mode = m
-// }
+func GoResponseToPy(gresp *requests.Response) *py.PyObject {
+	obj := py.PyDict_New()
+	py.PyDict_SetItem(obj, py.PyString_FromString("Status"), py.PyInt_FromLong(gresp.GResponse.StatusCode))
+	py.PyDict_SetItem(obj, py.PyString_FromString("Content"), py.PyString_FromString(gresp.Content()))
+	return obj
+}
 
-//GetMode 获取任务的模式
-func (p *Person) GetMode(m TypeMode) TypeMode {
-	return p.mode
+// Execute 执行
+func (person *Person) Execute() {
+	//TODO: Python的脚本函数, 与动态更新 返回的数据在python的脚本处理
+	for _, task := range person.Tasks {
+		for _, PResult := range task.ExecuteOnPlan() {
+			if task.Config.Setting.ResultProcessing != "" {
+				GoResponseToPy(PResult.Resp)
+			}
+		}
+
+	}
 }
--- person_test.go
+++ person_test.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"path/filepath"
 	"testing"
 )
 
 func TestPerson(t *testing.T) {
-	matches, err := filepath.Glob("task/*_config.py")
-	if err != nil {
-		panic(err)
-	}
-
-	var tasks []*Task
-	for _, match := range matches {
-		task := NewTask(match)
-		t.Error("task", task)
-		tasks = append(tasks, task)
-	}
+	p := NewPerson("task/*_config.py")
 
 }
